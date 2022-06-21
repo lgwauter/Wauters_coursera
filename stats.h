cc55b5f105e64bd4963a4ab6a2918dd23612ae5b
Laurens Wauters <wauterslaurens@gmail.com>
2022-06-21 15:17:26 +0200

    Adding code outline and documentation for C1M1 Assessment

--- stats.h
+++ stats.h
@@ -9,32 +9,121 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.h
+ * @brief Header file for the stats.c programming assignment.
  *
- * <Add Extended Description Here>
+ * This file contains the declarations for the 8 functions defined in stats.c
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Wauters Laurens
+ * @date June 19th, 2022
  *
  */
+
 #ifndef __STATS_H__
 #define __STATS_H__
 
 /* Add Your Declarations and Function Comments here */ 
 
 /**
- * @brief <Add Brief Description of Function Here>
+ * @brief Prints statistics of a given input array with a given length.
  *
- * <Add Extended Description Here>
+ * This function prints out the minimum, maximum, mean and median of the input array.
+ * It does not return anything.
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
+ * @param inputArray The input array, in unsigned char format
+ * @param size The length of the input array
  *
- * @return <Add Return Informaiton here>
+ * @return Null.
  */
+
+void print_statistics(unsigned char inputArray[], unsigned int size);
+
+/**
+ * @brief Prints a given input array with a given length to the screen.
+ *
+ * This function prints out the given input array.
+ *
+ * @param inputArray The input array, in unsigned char format
+ * @param size The length of the input array
+ *
+ * @return Null.
+ */
+
+
+void print_array(unsigned char inputArray[], unsigned int size);
+
+/**
+ * @brief Finds the minimum of an unsigned char array
+ *
+ * This function finds the minimum value of an unsigned char array, and returns an unsigned char, the
+ * minimum value of the array.
+ *
+ * @param inputArray The input array, in unsigned char format
+ * @param size The length of the input array
+ *
+ * @return An unsigned char representing the minimum value of inputArray.
+ */
+
+
+unsigned char find_median(unsigned char inputArray[], unsigned int size);
+
+/**
+ * @brief Finds the median of an unsigned char array
+ *
+ * This function finds the median value of an unsigned char array, and returns an unsigned char, the
+ * median value of the array.
+ *
+ * @param inputArray The input array, in unsigned char format
+ * @param size The length of the input array
+ *
+ * @return An unsigned char representing the median value of inputArray.
+ */
+
+
+unsigned float find_mean(unsigned char inputArray[], unsigned int size);
+
+/**
+ * @brief Finds the mean of an unsigned char array
+ *
+ * This function finds the mean of an unsigned char array, and returns an unsigned float, the
+ * mean value of the array.
+ *
+ * @param inputArray The input array, in unsigned char format
+ * @param size The length of the input array
+ *
+ * @return An unsigned float representing the mean value of inputArray.
+ */
+
+unsigned char find_minimum(unsigned char inputArray[], unsigned int size);
+
+
+/**
+ * @brief Finds the maximum of an unsigned char array
+ *
+ * This function finds the maximum value of an unsigned char array, and returns an unsigned char, the
+ * minimum value of the array.
+ *
+ * @param inputArray The input array, in unsigned char format
+ * @param size The length of the input array
+ *
+ * @return An unsigned char representing the maximum value of inputArray.
+ */
+
+unsigned char find_maximum(unsigned char inputArray[], unsigned int size);
+
+/**
+ * @brief Sorts an unsigned char array in descending order.
+ *
+ * This function takes an unsigned char array as input, and modifies it such that is becomes
+ * ordered from largest to smallest.
+ *
+ * @param inputArray The input array, in unsigned char format
+ * @param size The length of the input array
+ *
+ * @return Null.
+ */
+
+void sort_array(unsigned char inputArray[], unsigned int size);
 
 
 #endif /* __STATS_H__ */
